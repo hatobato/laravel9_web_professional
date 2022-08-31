b208a855942d99346480818c589968c4cfd6d664
hatobato <hatobato61@gmail.com>
2022-08-31 23:15:45 +0900

    update: Chapter2-14まで実装完了

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/tweet', \App\Http\Controllers\Tweet\IndexController::class);
+Route::get('/tweet', \App\Http\Controllers\Tweet\IndexController::class)->name('tweet.index');
+Route::post('/tweet/create', \App\Http\Controllers\Tweet\CreateController::class)->name('tweet.create');
