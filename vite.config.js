74fd2dfb10cd7593fd210e92f213cb45cef6291f
hatobato <hatobato61@gmail.com>
2022-09-10 01:50:58 +0900

    fix: ホットリロードが動作しない問題の解消

--- vite.config.js
+++ vite.config.js
@@ -12,8 +12,14 @@ export default defineConfig({
         }),
     ],
     server: {
+        // 以下のパラメータを指定しないとapp.css, app.jsのURLが0.0.0.0になってしまうため、明示的にlocalhostに変更
         hmr: {
             host: 'localhost'
+        },
+        // Windowsアプリでファイル編集した際に監視されない問題があるため、usePolling:trueにすることで監視を強制させる
+        // https://vitejs.dev/config/server-options.html#server-watch
+        watch: {
+            usePolling: true
         }
     }
 });
